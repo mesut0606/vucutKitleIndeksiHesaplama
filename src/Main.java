fa45b1fc027586807ae8001457394aa1ec7d5f56
mesut0606 <96373369+mesut0606@users.noreply.github.com>
2022-12-10 12:14:04 +0300

    https://www.patika.dev/tr

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+   double kg,boy,vki;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Boyunuzu girinz:");
+        boy = input.nextDouble();
+        System.out.println("Kilonuzu giriniz:");
+        kg = input.nextDouble();
+
+input.close();
+    vki = kg / ((boy*boy));
+        System.out.println("Vücut Kitle İndeksi :" + vki);
+
+
+    }
+}
